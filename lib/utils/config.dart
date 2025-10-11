a7ccb75328bdae19e19be25c73ed0e655f5f1d30
parichatr <bonlnnjvhk@gmail.com>
2025-10-12 02:48:39 +0700

    Revert "New"
    This reverts commit 5c1129f41a3f25fab1a0e2b8ceeae9425a6c5219, reversing
    changes made to da70f9f1b5fd23de132cf8d01e27296a57420a26.

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 //const String HOST = '192.168.0.39:8001';
-=======
-// const String HOST = '192.168.0.39:8001';
->>>>>>> ee197ebe873a117152d1d343d34dd277bba520da
 const String HOST = 'api-waiwan.mikenatcavon.com';
 
 const API_URL = 'https://$HOST';
