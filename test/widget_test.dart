075c52d95b3548c3bd197a91dad32a6348f70be9
Disezmike <mikenatchapon@gmail.com>
2025-10-15 09:57:38 +0700

    fix cant build

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:waiwan/main.dart';
+import 'package:waiwan_senior/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
